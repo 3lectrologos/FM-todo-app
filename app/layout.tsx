f0c7f6463ca7a909ac6a5f3452b5c3dd106d442c
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-03-07 19:01:48 +0100

    Fix banner img layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,21 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={josefinSans.className}>
+      <body
+        className={`${josefinSans.className} bg-lt_veryLightGray dark:bg-dt_veryDarkDesaturatedBlue`}
+      >
         <ThemeProviderWrapper>
           <div
             className={twMerge(
-              `flex flex-col items-center min-h-dvh bg-lt_veryLightGray dark:bg-dt_veryDarkDesaturatedBlue`
+              `absolute w-full h-[200px] tablet:h-[300px]`,
+              `bg-[url('/images/bg-mobile-light.jpg')] tablet:bg-[url('/images/bg-desktop-light.jpg')]`,
+              `dark:bg-[url('/images/bg-mobile-dark.jpg')] tablet:dark:bg-[url('/images/bg-desktop-dark.jpg')]`,
+              `bg-top bg-cover bg-no-repeat -z-10`
             )}
-          >
-            <div
-              className={twMerge(
-                `absolute w-full h-[200px] tablet:h-[300px]`,
-                `bg-[url('/images/bg-mobile-light.jpg')] tablet:bg-[url('/images/bg-desktop-light.jpg')]`,
-                `dark:bg-[url('/images/bg-mobile-dark.jpg')] tablet:dark:bg-[url('/images/bg-desktop-dark.jpg')]`,
-                `bg-top bg-cover bg-no-repeat`
-              )}
-            ></div>
+          />
+          <div className={twMerge(`flex flex-col items-center min-h-dvh`)}>
             {children}
           </div>
         </ThemeProviderWrapper>
