491f3e90bd9c5a365d4d832709180c13934d4379
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-05-20 18:16:23 +0200

    Avoid multiple PrismaClients during development

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,18 @@ import { PrismaClient } from '@prisma/client'
 import { Pool } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
-const neon = new Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaNeon(neon)
-export const prisma = new PrismaClient({ adapter })
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+function getPrismaClient() {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL })
+  const adapter = new PrismaNeon(neon)
+  return new PrismaClient({ adapter })
+}
+
+// NOTE: This is done to avoid multiple instances of PrismaClient in development.
+export const prisma = globalThis.prisma || getPrismaClient()
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma
+}
